f3e783c921d322ebf361d75289414117d11e9d56
alvarg93 <alvarglj@gmail.com>
2013-11-30 14:33:08 +0100

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,18 +1,28 @@
+#ifndef _FUNCTIONS
+#define _FUNCTIONS
+
+
 #include <iostream>
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
 #include <SDL_ttf.h>
 
+static int screen_width=1366, screen_height=768;
 static const int FPS = 60;
 static bool quit;
 static SDL_Renderer *sdlRenderer;
 static SDL_Window *sdlWindow;
-static int *msx,*msy;
+static int msx =0,msy = 0;
 static const Uint8* keys;
 static int SDL_BUTTON_WHEELUP = 4;
 static int SDL_BUTTON_WHEELDOWN = 5;
 static int gamestate = 1;
+static bool buttons[5];
+static TTF_Font *font;
+
+//controls
+
 
 int Init();
 int deInit();
@@ -21,6 +31,20 @@ void WaitFrame(int fps);
 void KeyHandler();
 void ProcessEvents();
 void DoEngine();
-void Render();
-void HandleMouseBtn();
+void Render(int gamestate);
+void HandleMouseBtnDown(Uint8 button);
+void HandleMouseBtnUp(Uint8 button);
 void StartEngine();
+
+template <class T>
+T Min (T a,T b) {
+ return (a<b?a:b);
+}
+
+template <class T>
+T Max (T a, T b) {
+ return (a>b?a:b);
+}
+
+
+#endif
